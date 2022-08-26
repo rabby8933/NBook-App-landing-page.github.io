0f571b96b69a1fe8fc7821758920f51058038d04
Fazle Rabby Khan <84661560+rabby8933@users.noreply.github.com>
2022-08-27 01:30:54 +0600

    NBook | App landing page

---
+++ js/main.js
@@ -0,0 +1,28 @@
+// Owl Carousel
+$(".owl-carousel").owlCarousel({
+  center: true,
+  items: 1,
+  loop: true,
+  nav: true,
+  margin: 10,
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 3,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 4,
+    },
+  },
+});
+
+// Counter up
+jQuery(document).ready(function ($) {
+  $(".counter").counterUp({
+    delay: 10,
+    time: 1000,
+  });
+});
